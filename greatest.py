16ae3b6f48ac09db60eb330648c912664d70e1cf
ankitalodha05 <aankitalodha05@gmail.com>
2024-08-22 19:11:07 +0530

    adding text

---
+++ greatest.py
@@ -0,0 +1,10 @@
+a = int(input("number 1 :"))
+b = int(input("number 2 :"))
+c = int(input("number 3 :"))
+
+if( a>=b and a>=c):
+    print ("number1 is greatest", a)
+elif(b>= c):
+    print("number2 is greatest", b)
+else: 
+    print("number3 is greatest", c)
